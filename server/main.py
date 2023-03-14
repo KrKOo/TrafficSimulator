11b3a9ea32f3a3da3f4dd4e51107d3eb1d6dab08
KrKOo <xkovac61@vutbr.cz>
2023-03-14 18:22:57 +0100

    Plot refactor

--- server/main.py
+++ server/main.py
@@ -1,12 +1,10 @@
-
-# import matplotlib
-# matplotlib.use( 'tkagg' )
 from matplotlib import pyplot as plt
-import numpy as np
 import simpy
+import random
+
 from modules import Parser
 from entities import Car
-import random
+from utils import plot
 
 
 if __name__ == "__main__":
@@ -18,47 +16,10 @@ if __name__ == "__main__":
     parser.apply_file("data/clean_brno.osm")
     parser.init_crossroads()
 
-    for idx, way in enumerate(parser.ways):
-        roads = way.roads
-        for road in roads:
-            x = [road.start.lng, road.end.lng]
-            y = [road.start.lat, road.end.lat]
-            if len(way.lanes.backward) == 0:  # oneway
-                plt.annotate(
-                    "",
-                    xy=([x[1], y[1]]),
-                    xytext=([x[0], y[0]]),
-                    arrowprops={
-                        "arrowstyle": "->",
-                        "linestyle": "-",
-                        "linewidth": 2,
-                        "shrinkA": 0,
-                        "shrinkB": 0,
-                        "color": "red",
-                    },
-                )
-            else:
-                plt.plot(x, y, "r", linestyle="-", linewidth=2)
-
-            plt.text(
-                (x[0] + x[1]) / 2,
-                (y[0] + y[1]) / 2,
-                f"{way.id}",
-                fontsize=10,
-                ha="center",
-                va="center",
-            )
-
-    for crossroad in parser.crossroads:
-        plt.plot(crossroad.node.pos.lng, crossroad.node.pos.lat, "bo")
-        plt.annotate(
-            f"{crossroad.id}",
-            (crossroad.node.pos.lng, crossroad.node.pos.lat),
-        )
-        # print(crossroad)
-        # print("----------------")
-
     car1 = Car(env, parser.ways[13], 0, 50)
 
     env.run(until=100000)
+
+    plot.plot_ways(plt, parser.ways)
+    plot.plot_crossroads(plt, parser.crossroads)
     plt.show()
--- server/utils/plot.py
+++ server/utils/plot.py
@@ -1,12 +1,13 @@
-from entities import Road, Way
+from entities import Way, Crossroad
 import matplotlib
 
 
 def plot_ways(plt: matplotlib.pyplot, ways: list[Way]):
     for way in ways:
-        for road in way.roads:
+        for idx, road in enumerate(way.roads):
             x = [road.start.lng, road.end.lng]
             y = [road.start.lat, road.end.lat]
+
             if len(way.lanes.backward) == 0:  # oneway
                 plt.annotate(
                     "",
@@ -24,13 +25,20 @@ def plot_ways(plt: matplotlib.pyplot, ways: list[Way]):
             else:
                 plt.plot(x, y, "r", linestyle="-", linewidth=2)
 
-            plt.text(
-                (x[0] + x[1]) / 2,
-                (y[0] + y[1]) / 2,
-                f"{way.id}, {way.length}",
-                fontsize=10,
-                ha="center",
-                va="center",
-            )
+            if idx == len(way.roads) // 2:
+                plt.text(
+                    (x[0] + x[1]) / 2,
+                    (y[0] + y[1]) / 2,
+                    f"{way.id}",
+                    fontsize=10,
+                    ha="center",
+                    va="center",
+                )
 
-    plt.show()
+def plot_crossroads(plt: matplotlib.pyplot, crossroads: list[Crossroad]):
+    for crossroad in crossroads:
+        plt.plot(crossroad.node.pos.lng, crossroad.node.pos.lat, "bo")
+        plt.annotate(
+            f"{crossroad.id}",
+            (crossroad.node.pos.lng, crossroad.node.pos.lat),
+        )
