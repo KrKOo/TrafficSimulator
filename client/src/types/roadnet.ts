c2a539df3923b2ded37abe85cc0435ce4b430c23
KrKOo <xkovac61@vutbr.cz>
2023-04-05 12:32:12 +0200

    File structure modification

---
+++ client/src/types/roadnet.ts
@@ -0,0 +1,49 @@
+export enum Turns {
+  none = 0,
+  left,
+  right,
+  through,
+  merge_to_right,
+  merge_to_left,
+  slight_right,
+  slight_left,
+}
+
+export interface Event {
+  time: number;
+  car_id: number;
+  way_id: number;
+  lane_id: number;
+  position: number;
+  speed: number;
+}
+
+export interface Node {
+  id: bigint;
+  lat: number;
+  lng: number;
+}
+
+export interface Lane {
+  id: number;
+  is_forward: boolean;
+  turns: Turns[];
+}
+
+export interface Way {
+  id: number;
+  max_speed: number;
+  nodes: Node[];
+  lanes: Lane[];
+}
+
+export interface Crossroad {
+  id: number;
+  node_id: bigint;
+}
+
+export interface Simulation {
+  ways: Way[];
+  crossroads: Crossroad[];
+  events: Event[];
+}
